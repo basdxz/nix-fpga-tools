3e4188467af71be2a37f85245650bff4de58c54e
FalsePattern <me@falsepattern.com>
2025-01-15 22:11:44 +0100

    fix: update flake to decouple overlay from system nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,41 +2,29 @@
   description = "FPGA tools for Nix.";
 
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, flake-utils, ... }:
     let
       # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-
-    in
-    {
-      # A Nixpkgs overlay.
-      overlay = self: super: {
-        xilinx-ise = super.callPackage ./pkgs/xilinx-ise { };
-        xilinx-udev-rules = super.callPackage ./pkgs/xilinx-ise/udev-rules.nix { };
-      };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) xilinx-ise xilinx-udev-rules;
-        });
-
-      # Add the wrapper to apps for nix run.
-      apps = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in {
+      systems = [ "x86_64-linux" ];
+      outputs = flake-utils.lib.eachSystem systems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = {
+          xilinx-ise = pkgs.callPackage ./pkgs/xilinx-ise { };
+          xilinx-udev-rules = pkgs.callPackage ./pkgs/xilinx-ise/udev-rules.nix { };
+        };
+        overlay = self: super: {
+          xilinx-ise = packages.xilinx-ise;
+          xilinx-udev-rules = packages.xilinx-udev-rules;
+        };
+        apps = {
           xilinx-ise = {
             type = "app";
-            program = "${pkgs.xilinx-ise}/bin/xilinx-ise";
+            program = "${packages.xilinx-ise}/bin/xilinx-ise";
           };
-        });
-    };
+        };
+      });
+    in outputs;
 }
