db49fbbc1cea0a0b067f2b35f44433dc0f870c4a
Ven <tudurap.com@gmail.com>
2025-06-01 16:06:19 +0100

    fix: udev rules group

--- pkgs/xilinx-ise/udev-rules.nix
+++ pkgs/xilinx-ise/udev-rules.nix
@@ -8,7 +8,7 @@ pkgs.writeTextFile {
   destination = "/etc/udev/rules.d/10-xilinx.rules";
   # Originally derived from: /opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/xusbdfwu.rules
   text = ''
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0008", MODE="0660", TAG+="uaccess", GROUP+="plugdev"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0008", MODE="0660", TAG+="uaccess", GROUP="plugdev"
     SUBSYSTEMS=="usb", ENV{DEVTYPE}=="usb_device", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0007", RUN+="${pkgs.fxload}/bin/fxload -v -t fx2 -I ${fw}/share/xusbdfwu.hex -p $env{BUSNUM},$env{DEVNUM}"
     SUBSYSTEMS=="usb", ENV{DEVTYPE}=="usb_device", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0009", RUN+="${pkgs.fxload}/bin/fxload -v -t fx2 -I ${fw}/share/xusb_xup.hex -p $env{BUSNUM},$env{DEVNUM}"
     SUBSYSTEMS=="usb", ENV{DEVTYPE}=="usb_device", ACTION=="add", ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000d", RUN+="${pkgs.fxload}/bin/fxload -v -t fx2 -I ${fw}/share/xusb_emb.hex -p $env{BUSNUM},$env{DEVNUM}"
